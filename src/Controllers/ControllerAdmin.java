f97a8d899e19f8847d8fe2b87eb0a13ceabe9bc6
nydia <nydia@LAPTOP-N2TCBQFC>
2023-11-23 20:57:46 -0500

    Desarrollo de controladores. Mi parte.

--- src/Controllers/ControllerAdmin.java
+++ src/Controllers/ControllerAdmin.java
@@ -1,12 +1,58 @@
 package Controllers;
 
 import Vista.UIADMIN;
+import Vista.UIADMININFORMACION;
+import Vista.UIADMINUSUARIOS;
+import Vista.UITUTORIALES;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ControllerAdmin {
-    private final UIADMIN adminVista;
 
-    public ControllerAdmin(Vista.UIADMIN adminVista) {
-        this.adminVista = adminVista;
-        this.adminVista.setVisible(true);
+    private UIADMIN vista;
+
+    public ControllerAdmin(UIADMIN vista) {
+        this.vista = vista;
+        this.vista.iniciobtn.addActionListener(new InicioListener());
+        this.vista.tutosBTN.addActionListener(new TutorialesListener());
+        this.vista.adminInfoBTN.addActionListener(new AdminInfoListener());
+        this.vista.adminUsuarioBTN.addActionListener(new AdminUsuariosListener());
+    }
+
+    private class InicioListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UIADMIN inicio = new UIADMIN();
+            inicio.setVisible(true);
+            vista.setVisible(false);
+        }
+    }
+
+    private class TutorialesListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UITUTORIALES tutoriales = new UITUTORIALES();
+            tutoriales.setVisible(true);
+            vista.setVisible(false);
+        }
+    }
+
+    private class AdminInfoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UIADMININFORMACION adminInfo = new UIADMININFORMACION();
+            adminInfo.setVisible(true);
+            vista.setVisible(false);
+        }
+    }
+
+    private class AdminUsuariosListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UIADMINUSUARIOS adminUsuarios = new UIADMINUSUARIOS();
+            adminUsuarios.setVisible(true);
+            vista.setVisible(false);
+        }
     }
 }
--- src/Controllers/ControllerUser.java
+++ src/Controllers/ControllerUser.java
@@ -1,12 +1,37 @@
 package Controllers;
 
 import Vista.UIUSER;
+import Vista.UITUTORIALES;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ControllerUser {
-    private final UIUSER userVista;
 
-    public ControllerUser(Vista.UIUSER userVista) {
-        this.userVista = userVista;
-        this.userVista.setVisible(true);
+    private UIUSER vista;
+
+    public ControllerUser(UIUSER vista) {
+        this.vista = vista;
+        this.vista.iniciobtn.addActionListener(new InicioListener());
+        this.vista.tutosBTN.addActionListener(new TutorialesListener());
+    }
+
+    private class InicioListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UIUSER inicio = new UIUSER();
+            inicio.setVisible(true);
+            vista.setVisible(false);
+        }
+    }
+
+    private class TutorialesListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UITUTORIALES tutoriales = new UITUTORIALES();
+            tutoriales.setVisible(true);
+            vista.setVisible(false);
+        }
     }
 }
+
